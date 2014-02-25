e6427fc74205b93e598e35158f67396122f695c0
zhanglihua <zhanglihuahuahua@126.com>
2014-02-25 22:46:46 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helmholtz::Application.config.secret_token = '514f6e268bd032cce5d909f84b5ba473be4d3625798dc181e96b00edf6d17f2ca6d8ac92d1c934165bd204360d9950c8c6495fe9bacddca9a87e46b5c6e93b41'
