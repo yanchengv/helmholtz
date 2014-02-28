a34a79dfd1f5d291844b53e0322fd8dc860a2e47
zhanglihua <zhanglihuahuahua@126.com>
2014-02-28 09:23:29 +0800

    success

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def custommenu
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+
+    presenter.dom_id = 'menu'
+    presenter.css = 'menu clearfix'
+    presenter.menu_tag = :nav
+    presenter.list_tag = :ul
+    presenter.list_item_tag = :li
+    presenter.selected_css = :current
+    presenter.first_css = :first
+    presenter.last_css = :last
+    #presenter.list_tag_css = 'sfmenu'
+    presenter
+  end
 end
