d69024d29fd102a41e23e46d9780688fd98677a5
zhanglihua <zhanglihuahuahua@126.com>
2014-03-05 22:50:21 +0800

    sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,33 @@ module ApplicationHelper
     #presenter.list_tag_css = 'sfmenu'
     presenter
   end
+  def footer_menu
+    menu_items = refinery_menu_pages
+
+    presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+    presenter.dom_id = "footer_menu"
+    presenter.css = "footer_menu"
+    presenter.menu_tag = :div
+    presenter.list_tag = :ul
+    presenter.list_item_tag = :li
+    presenter.selected_css = :current
+    presenter.first_css = nil
+    presenter.last_css = nil
+    presenter
+  end
+  def side_menu
+    menu_items = refinery_menu_pages
+
+    presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+    presenter.dom_id = "side_menu"
+    presenter.css = "side_menu"
+    presenter.menu_tag = :div
+    presenter.list_tag = :ul
+    presenter.list_item_tag = :li
+    presenter.selected_css = :current
+    presenter.first_css = nil
+    presenter.last_css = nil
+    presenter.roots = refinery_menu_pages.select{|p| p.parent_id == @page.root.id}
+    presenter
+  end
 end
