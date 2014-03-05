b7a0a74ad81562fd104be2ac37c3edc5d1c944b9
zhanglihua <zhanglihuahuahua@126.com>
2014-03-06 00:49:34 +0800

    list item css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -28,9 +28,25 @@ module ApplicationHelper
     presenter
   end
   def side_menu
-    menu_items = refinery_menu_pages
-
-    presenter = Refinery::Pages::MenuPresenter.new(menu_items, self)
+    #Refinery::Menu.new(refinery_menu_pages.detect{ |item| item.original_id == page.id }.children)
+    #refinery_menu_pages.select{|item| item.parent_id == @page.root.id or item.parent_id == @page.id}
+=begin
+    menu_items = []
+    for item in refinery_menu_pages.items
+      if item.parent_id == @page.root.id
+        menu_items.append(item)
+        next
+      end
+      if item.parent_id == @page.id
+        menu_items.append(item)
+        next
+      end
+    end
+    menu = Refinery::Menu.new
+    menu.items = menu_items
+=end
+    menu = refinery_menu_pages
+    presenter = Refinery::Pages::MenuPresenter.new(menu, self)
     presenter.dom_id = "side_menu"
     presenter.css = "side_menu"
     presenter.menu_tag = :div
