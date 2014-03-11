2bd957520eea1516120a4f17289e69085657ad04
zhanglihua <zhanglihuahuahua@126.com>
2014-03-11 18:00:15 +0800

    sitemap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,6 +13,7 @@ module ApplicationHelper
     #presenter.list_tag_css = 'sfmenu'
     presenter
   end
+
   def footer_menu
     menu_items = refinery_menu_pages
 
@@ -27,6 +28,7 @@ module ApplicationHelper
     presenter.last_css = nil
     presenter
   end
+
   def side_menu
     #Refinery::Menu.new(refinery_menu_pages.detect{ |item| item.original_id == page.id }.children)
     #refinery_menu_pages.select{|item| item.parent_id == @page.root.id or item.parent_id == @page.id}
@@ -58,10 +60,13 @@ module ApplicationHelper
     presenter.roots = refinery_menu_pages.select{|p| p.parent_id == @page.root.id}
     presenter
   end
+
   def path_map
     pathlist = []
     parent_root_page = @page.root
-    pathlist.append(parent_root_page)
+    if parent_root_page.link_url != '/'
+      pathlist.append(parent_root_page)
+    end
     if @page.parent_id == nil
       return pathlist
     end
@@ -78,15 +83,33 @@ module ApplicationHelper
       return pathlist
     end
   end
+
+  def site_map
+    presenter = Refinery::Pages::MenuPresenter.new(refinery_menu_pages, self)
+
+    presenter.dom_id = 'sitemap'
+    presenter.css = 'sitemap clearfix'
+    presenter.menu_tag = :nav
+    presenter.list_tag = :ul
+    presenter.list_item_tag = :li
+    presenter.selected_css = :current
+    presenter.first_css = :first
+    presenter.last_css = :last
+    #presenter.list_tag_css = 'sfmenu'
+    presenter
+  end
+
   def newest_pages
     f = Refinery::Page.fast_menu
     for item in f
 
     end
   end
+
   def top_page
     Refinery::Page.top_pages[0]
   end
+
   def sticky_pages
     Refinery::Page.sticky_pages
   end
