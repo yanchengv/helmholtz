da2d59ae2346aebc6512d70209a2bc3aa0711369
zhanglihua <zhanglihuahuahua@126.com>
2014-03-11 18:46:26 +0800

    unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,7 +3,7 @@ worker_processes 4
 APP_PATH = "/home/ubuntu/helmholtz"
 working_directory APP_PATH
 
-listen "/tmp/unicorn.helmholtz.sock", :backlog => 64
+listen APP_PATH + "/tmp/unicorn.helmholtz.sock", :backlog => 64
 pid APP_PATH + "/tmp/pids/unicorn.pid"
 stderr_path APP_PATH + "/log/unicorn.stderr.log"
 stdout_path APP_PATH + "/log/unicorn.stderr.log"
